a47f6e4ffe79cb6fb5a717d1d6df39558e52566b
Shridharbhandar <bhandar1994@gmail.com>
2024-04-06 22:57:58 +0530

    Updating the branch fail-both by having syntax error

--- train.py
+++ train.py
@@ -11,9 +11,7 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-
 model == RandomForestClassifier().fit(X, y)
-
 
 
 with open("model.pkl", 'wb') as f:
